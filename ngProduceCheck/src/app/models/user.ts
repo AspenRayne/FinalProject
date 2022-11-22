a96ce1226162ef25e5a3c73b26f2f061b4d68922
Cullen Rafferty <cullen1882@gmail.com>
2022-11-22 16:03:53 -0500

    user model, auth serv, config routing

---
+++ ngProduceCheck/src/app/models/user.ts
@@ -0,0 +1,32 @@
+import { TmplAstRecursiveVisitor } from "@angular/compiler";
+
+export class User {
+
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+
+
+  constructor(
+
+    id: number = 0,
+    username: string = '',
+    password: string = '',
+    email: string = '',
+    role: string = ''
+  )
+
+  {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+
+  }
+
+
+
+}
