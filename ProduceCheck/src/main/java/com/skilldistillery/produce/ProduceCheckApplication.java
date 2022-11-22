07130e734c0ee54178f212f0e19c790b45110077
Aspen Newman <AspenNewman7@gmail.com>
2022-11-22 10:50:57 -0700

    Create Boot/REST project, connect project dependency

---
+++ ProduceCheck/src/main/java/com/skilldistillery/produce/ProduceCheckApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduceCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduceCheckApplication.class, args);
+	}
+
+}
---
+++ ProduceCheck/src/test/java/com/skilldistillery/produce/ProduceCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.produce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
