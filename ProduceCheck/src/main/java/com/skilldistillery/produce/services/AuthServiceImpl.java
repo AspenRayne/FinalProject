319b2cf9fd19206a296dbcad7c02499c0ea2b152
Nolan Urban <nolanurban@gmail.com>
2022-11-22 14:21:16 -0600

    Ran full smoketest: auth 200 returned -- looks gtg.

--- ProduceCheck/src/main/java/com/skilldistillery/produce/services/AuthServiceImpl.java
+++ ProduceCheck/src/main/java/com/skilldistillery/produce/services/AuthServiceImpl.java
@@ -5,30 +5,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.produce.entities.User;
+import com.skilldistillery.produce.repositories.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
-//	@Autowired
-//	private UserRepository userRepo;
+	@Autowired
+	private UserRepository userRepo;
 
 	@Autowired
 	private PasswordEncoder encoder;
 
 	@Override
 	public User register(User user) {
-//		String encodedPW = encoder.encode(user.getPassword());
-//		user.setPassword(encodedPW);
-//		user.setEnabled(true);
-//		user.setRole("standard");
-//		userRepo.saveAndFlush(user);
+		String encodedPW = encoder.encode(user.getPassword());
+		user.setPassword(encodedPW);
+		user.setEnabled(true);
+		user.setRole("standard");
+		userRepo.saveAndFlush(user);
 		return user;
 	}
 
 	@Override
 	public User getUserByUsername(String username) {
-//		return userRepo.findByUsername(username);
-		return null; // FIXME
+		return userRepo.findByUsername(username);
 	}
 
 }
