0dcdd3e99b92bb3a2bf7b32a2e682bce18b17b3e
Cullen Rafferty <cullen1882@gmail.com>
2022-11-23 17:53:32 -0500

    company entity junit

---
+++ JPAVeggieTells/src/main/java/com/skilldistillery/produce/entities/Company.java
@@ -0,0 +1,82 @@
+package com.skilldistillery.produce.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@Column(name = "api_host_url")
+	private String apiHostUrl;
+
+	public Company() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getApiHostUrl() {
+		return apiHostUrl;
+	}
+
+	public void setApiHostUrl(String apiHostUrl) {
+		this.apiHostUrl = apiHostUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Company [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", apiHostUrl=");
+		builder.append(apiHostUrl);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
