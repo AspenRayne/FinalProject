6973ec20a584881e9f2e5a3d7f47a2950056fe48
Aspen Newman <AspenNewman7@gmail.com>
2022-11-23 16:40:34 -0700

    Added MTO, MapsId and JoinColumns for RecipeReaction

--- JPAVeggieTells/src/main/java/com/skilldistillery/produce/entities/RecipeReaction.java
+++ JPAVeggieTells/src/main/java/com/skilldistillery/produce/entities/RecipeReaction.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 @Table(name = "recipe_reaction")
@@ -14,6 +17,16 @@ public class RecipeReaction {
 
 	@EmbeddedId
 	private RecipeReactionId id;
+
+	@ManyToOne
+	@JoinColumn(name = "recipe_id")
+	@MapsId(value = "recipeId")
+	private Recipe recipe;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@MapsId(value = "userId")
+	private User user;
 
 	@Column(name = "reaction_date")
 	private LocalDateTime reactionDate;
@@ -28,6 +41,22 @@ public class RecipeReaction {
 
 	public void setId(RecipeReactionId id) {
 		this.id = id;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	public LocalDateTime getReactionDate() {
