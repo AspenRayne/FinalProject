e756726f02d2a69022124239eceeaf1aa6321abc
Aspen Newman <AspenNewman7@gmail.com>
2022-11-28 14:24:16 -0700

    Stubbed out components on Front End in Angular

--- ngProduceCheck/src/app/app.module.ts
+++ ngProduceCheck/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    RecipeComponent,
+    LogoutComponent,
+    NotFoundComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
