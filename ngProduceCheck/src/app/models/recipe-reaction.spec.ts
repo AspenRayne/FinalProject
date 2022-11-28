e92904e828bb9e5d3158c05aa6ead3d4309047fb
Aspen Newman <AspenNewman7@gmail.com>
2022-11-28 15:48:33 -0700

    Stubbed out models for user collections

---
+++ ngProduceCheck/src/app/models/recipe-reaction.spec.ts
@@ -0,0 +1,7 @@
+import { RecipeReaction } from './recipe-reaction';
+
+describe('RecipeReaction', () => {
+  it('should create an instance', () => {
+    expect(new RecipeReaction()).toBeTruthy();
+  });
+});
