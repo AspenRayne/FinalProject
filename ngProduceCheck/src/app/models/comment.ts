dc7f5792d7bd6a1788fa43b0ae2305cc7be1b528
Cullen Rafferty <cullen1882@gmail.com>
2022-11-28 18:17:32 -0500

    comment model in angular built

--- ngProduceCheck/src/app/models/comment.ts
+++ ngProduceCheck/src/app/models/comment.ts
@@ -1,2 +1,35 @@
+import { Recipe } from "./recipe";
+import { User } from "./user";
+
 export class Comment {
+
+  id: number;
+  commentDate: string;
+  comment: string;
+  user: User;
+  recipe: Recipe;
+  replyComment: Comment;
+  comments: Comment[];
+
+  constructor(
+
+    id: number = 0,
+    commentDate: string = '',
+    comment: string = '',
+    user: User = new User,
+    recipe: Recipe = new Recipe,
+    replyComment: Comment = new Comment,
+    comments: Comment[] = []
+  )
+  {
+    this.id = id;
+    this.commentDate = commentDate;
+    this.comment = comment;
+    this.user = user;
+    this.recipe = recipe;
+    this.replyComment = replyComment;
+    this.comments = comments
+
+
+  }
 }
