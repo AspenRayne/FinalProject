344ca72b3201d85fcb5d70f48482ed7cf71b9dc0
Aspen Newman <AspenNewman7@gmail.com>
2022-11-28 15:54:42 -0700

    Configured Store model in Angular

--- ngProduceCheck/src/app/models/store.ts
+++ ngProduceCheck/src/app/models/store.ts
@@ -1,2 +1,24 @@
 export class Store {
+  id: number;
+  street1: string;
+  street2: string;
+  city: string;
+  state: string;
+  zipcode: string;
+
+  constructor(
+    id: number = 0,
+    street1: string = '',
+    street2: string = '',
+    city: string = '',
+    state: string = '',
+    zipcode: string = ''
+  ) {
+    this.id = id;
+    this.street1 = street1;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+  }
 }
