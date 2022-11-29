c5fbcf23d44ddc5086a800edef82d6a815479c62
Aspen Newman <AspenNewman7@gmail.com>
2022-11-28 17:03:27 -0700

    Changed some perferences in models

--- ngProduceCheck/src/app/models/recipe-ingredient.ts
+++ ngProduceCheck/src/app/models/recipe-ingredient.ts
@@ -1,20 +1,18 @@
-import { Ingredient } from "./ingredient";
-import { Recipe } from "./recipe";
+import { Ingredient } from './ingredient';
+import { Recipe } from './recipe';
 
 export class RecipeIngredient {
   id: number;
   recipe: Recipe;
-  ingredient: Ingredient = new Ingredient;
-  measurement: string | undefined;
-
+  ingredient: Ingredient;
+  measurement: string;
 
   constructor(
     id: number = 0,
-    recipe: Recipe = new Recipe,
-    ingredient: Ingredient = new Ingredient,
-    measurement?: string | undefined
-  )
-  {
+    recipe: Recipe = new Recipe(),
+    ingredient: Ingredient = new Ingredient(),
+    measurement: string = ''
+  ) {
     this.id = id;
     this.recipe = recipe;
     this.ingredient = ingredient;
--- ngProduceCheck/src/app/models/user.ts
+++ ngProduceCheck/src/app/models/user.ts
@@ -1,32 +1,56 @@
-import { TmplAstRecursiveVisitor } from "@angular/compiler";
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
+import { Recipe } from './recipe';
+import { RecipeReaction } from './recipe-reaction';
+import { Store } from './store';
 
 export class User {
-
   id: number;
   username: string;
   password: string;
   email: string;
   role: string;
-
+  createdDate: string;
+  loginTimestamp: string;
+  aboutMe: string;
+  profilePic: string;
+  firstName: string;
+  lastName: string;
+  recipes: Recipe[];
+  recipeReactions: RecipeReaction[];
+  comments: Comment[];
+  stores: Store[];
 
   constructor(
-
     id: number = 0,
     username: string = '',
     password: string = '',
     email: string = '',
-    role: string = ''
-  )
-
-  {
+    role: string = '',
+    createdDate: string = '',
+    loginTimestamp: string = '',
+    aboutMe: string = '',
+    profilePic: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    recipes: Recipe[] = [],
+    recipeReactions: RecipeReaction[] = [],
+    comments: Comment[] = [],
+    stores: Store[] = []
+  ) {
     this.id = id;
     this.username = username;
     this.password = password;
     this.email = email;
     this.role = role;
-
+    this.createdDate = createdDate;
+    this.loginTimestamp = loginTimestamp;
+    this.aboutMe = aboutMe;
+    this.profilePic = profilePic;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.recipes = recipes;
+    this.recipeReactions = recipeReactions;
+    this.comments = comments;
+    this.stores = stores;
   }
-
-
-
 }
