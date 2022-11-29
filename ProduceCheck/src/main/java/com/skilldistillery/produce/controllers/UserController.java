9e85c682b9ac941ecdf0d34c78fec20fb0c00d30
Cullen Rafferty <cullen1882@gmail.com>
2022-11-29 13:30:01 -0500

    user controller complete

--- ProduceCheck/src/main/java/com/skilldistillery/produce/controllers/UserController.java
+++ ProduceCheck/src/main/java/com/skilldistillery/produce/controllers/UserController.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +33,44 @@ public class UserController {
 	
 	@GetMapping("users/{id}")
 	public User find(@PathVariable int id, HttpServletResponse res) {
+		
 		return userServ.show(id);
 	}
-
+	
+	@PostMapping("users")
+	public User create(@RequestBody User user, HttpServletResponse res) {
+		User newUser = null;
+		try {
+			newUser = userServ.create(user);
+			res.setStatus(201);
+		}catch(Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
+		return newUser;
+	}
+	
+	@PutMapping("users/{id}")
+	public User update(@PathVariable int id, @RequestBody User user, HttpServletResponse res) {
+		User updateUser = null;
+		try {
+			updateUser = userServ.update(id, user);
+			res.setStatus(201);
+		} catch(Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
+		return updateUser;
+	}
+	@DeleteMapping("users/{id}")
+	public boolean destroy(@PathVariable int id, HttpServletResponse res) {
+		boolean deleted = userServ.delete(id);
+		if(deleted) {
+			res.setStatus(204);
+			return deleted;
+		} else {
+			res.setStatus(400);
+			return false;
+		}
+	}
 }
