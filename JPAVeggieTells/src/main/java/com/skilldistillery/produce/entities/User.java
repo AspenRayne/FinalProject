25f98a180d4d24eab946cf6e1c34c7cf0294f789
Cullen Rafferty <cullen1882@gmail.com>
2022-12-02 22:11:42 -0500

    email added to java user, sql udated

--- JPAVeggieTells/src/main/java/com/skilldistillery/produce/entities/User.java
+++ JPAVeggieTells/src/main/java/com/skilldistillery/produce/entities/User.java
@@ -34,6 +34,8 @@ public class User {
 	private Boolean enabled;
 
 	private String role;
+	
+	private String email;
 
 	@Column(name = "created_date")
 	@CreationTimestamp
@@ -118,6 +120,14 @@ public class User {
 
 	public void setRole(String role) {
 		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public LocalDateTime getCreatedDate() {
@@ -321,9 +331,9 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
-				+ ", role=" + role + ", createdDate=" + createdDate + ", loginTimestamp=" + loginTimestamp
-				+ ", aboutMe=" + aboutMe + ", profilePic=" + profilePic + ", firstName=" + firstName + ", lastName="
-				+ lastName + "]";
+				+ ", role=" + role + ", email=" + email + ", createdDate=" + createdDate + ", loginTimestamp="
+				+ loginTimestamp + ", aboutMe=" + aboutMe + ", profilePic=" + profilePic + ", firstName=" + firstName
+				+ ", lastName=" + lastName + "]";
 	}
 
 }
