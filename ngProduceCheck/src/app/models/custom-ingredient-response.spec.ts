213715d4455d8f18581d88c259919b7fd77938fb
Aspen Newman <AspenNewman7@gmail.com>
2022-12-03 18:55:59 -0700

    Stubbed out ingredient service

---
+++ ngProduceCheck/src/app/models/custom-ingredient-response.spec.ts
@@ -0,0 +1,7 @@
+import { CustomIngredientResponse } from './custom-ingredient-response';
+
+describe('CustomIngredientResponse', () => {
+  it('should create an instance', () => {
+    expect(new CustomIngredientResponse()).toBeTruthy();
+  });
+});
---
+++ ngProduceCheck/src/app/models/custom-ingredient-statistics.ts
@@ -0,0 +1,28 @@
+class IngredientStatistics {
+  salePrice: number;
+  price: number;
+  inStore: boolean;
+  upc: string;
+  availability: string;
+
+  constructor(
+    salePrice: number = 0,
+    price: number = 0,
+    inStore: boolean = false,
+    upc: string = '',
+    availability: string = ''
+  ) {
+    this.salePrice = salePrice;
+    this.price = price;
+    this.inStore = inStore;
+    this.upc = upc;
+    this.availability = availability;
+  }
+}
+export class CustomIngredientStatistics {
+  statMap: Map<String, IngredientStatistics>;
+
+  constructor(statMap: Map<string, IngredientStatistics>) {
+    this.statMap = statMap;
+  }
+}
