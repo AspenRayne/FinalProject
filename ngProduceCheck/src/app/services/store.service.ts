6029ae0b5d73cafff0dd9e78525786e33e71d248
Aspen Newman <AspenNewman7@gmail.com>
2022-12-03 18:00:31 -0700

    Configured Store service

---
+++ ngProduceCheck/src/app/services/store.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NO_ERRORS_SCHEMA } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Store } from '../models/store';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  private baseUrl = 'http://localhost:8088/api/favoriteStore'
+  private searchUrl = 'http://localhost:8088/api/stores';
+  private indexUrl = 'http://localhost:8088/api/favoriteStores'
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+
+  getHttpOptions(){
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  getStoreLocations(zipcode: String): Observable<Store[]>{
+    return this.http.get<Store[]>(`${this.searchUrl}/${zipcode}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('StoreService.getStoreLocations():error searching stores: ' + err)
+        );
+      })
+    )
+  }
+
+  getUserStores(): Observable<Store[]>{
+    return this.http.get<Store[]>(`${this.indexUrl}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('StoreService.getUserStores():error retrieving stores: ' + err)
+        );
+      })
+    )
+  }
+
+  setFavoriteStore(store: Store): Observable<Store>{
+    return this.http.post<Store>(this.baseUrl, store, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('StoreService.setFavoriteStore():error saving store: ' + err)
+        );
+      })
+    )
+  }
+
+  unsaveStore(id:number) {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('StoreService.unsaveStore():error unsaving store: ' + err)
+        );
+      })
+    )
+  }
+
+}
